086819eb86dedb4513b515808e7045635b279675
ILIA KANASHVILI <146118050+kanashvili7@users.noreply.github.com>
2023-11-17 16:34:39 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+//pirveli//
+for (let i = 0; i <= 100; i++) {
+	console.log(i);
+  };
+  //meore//
+  
+  let y=0;
+while(y<50){
+  y++;
+  console.log(y);
+}
+   //mesame//
+let users=["tatia", "ilia", "gvanca", "beka", "nika"];
+ users.pop( );
+ users.unshift("mariam");
+ console.log (users);
+ users.push("nino");
+ users.shift( );
+ console.log (users);
+
+ //meotxe//
+ let arr = [];
+for(let i= 1; i <=10000; i++){
+    arr.push(i*i);
+}
+console.log(arr);
+
+//mexute//
+let numbers = [3, 5, 7, 9, 11];
+let sum = 0;
+for(let i = 0; i < numbers.length; i++){
+    sum += numbers[i];
+}
+console.log(sum);
+//meekvse//
+//meshvide//
+function convertToLowerCase(uppercaseString) {
+    return uppercaseString.toLowerCase();
+}
+
+let uppercaseString = "MY NAME IS JOHN";
+let lowercaseString = convertToLowerCase(uppercaseString);
+console.log(lowercaseString);
+//merve//
